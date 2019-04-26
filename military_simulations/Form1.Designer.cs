2c8d34b181f3d993de99375d81678c49ba008b2d
Luke Geyser <geyserlg@gmail.com>
2019-04-26 22:41:56 +0200

    Chnages

--- military_simulations/Form1.Designer.cs
+++ military_simulations/Form1.Designer.cs
@@ -29,6 +29,16 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tlt_Fuel = new System.Windows.Forms.ToolStripProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnl_Obstacles = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_enemyemplacements = new System.Windows.Forms.Button();
+            this.btn_RPGsquads = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.pnl_Obstacles.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -43,27 +53,124 @@
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
             this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseClick);
             // 
+            // btn_close
+            // 
+            this.btn_close.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_close.Location = new System.Drawing.Point(1316, 791);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(138, 60);
+            this.btn_close.TabIndex = 1;
+            this.btn_close.Text = "Close App";
+            this.btn_close.UseVisualStyleBackColor = false;
+            this.btn_close.Click += new System.EventHandler(this.Btn_close_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tlt_Fuel});
+            this.toolStrip1.Location = new System.Drawing.Point(913, 826);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(214, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tlt_Fuel
+            // 
+            this.tlt_Fuel.Name = "tlt_Fuel";
+            this.tlt_Fuel.Size = new System.Drawing.Size(200, 22);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(924, 804);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Fuel Amount:";
+            // 
+            // pnl_Obstacles
+            // 
+            this.pnl_Obstacles.Controls.Add(this.btn_RPGsquads);
+            this.pnl_Obstacles.Controls.Add(this.btn_enemyemplacements);
+            this.pnl_Obstacles.Controls.Add(this.label2);
+            this.pnl_Obstacles.Location = new System.Drawing.Point(1254, 12);
+            this.pnl_Obstacles.Name = "pnl_Obstacles";
+            this.pnl_Obstacles.Size = new System.Drawing.Size(200, 366);
+            this.pnl_Obstacles.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "OBSTACLES";
+            // 
+            // btn_enemyemplacements
+            // 
+            this.btn_enemyemplacements.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_enemyemplacements.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_enemyemplacements.Location = new System.Drawing.Point(44, 68);
+            this.btn_enemyemplacements.Name = "btn_enemyemplacements";
+            this.btn_enemyemplacements.Size = new System.Drawing.Size(112, 86);
+            this.btn_enemyemplacements.TabIndex = 6;
+            this.btn_enemyemplacements.UseVisualStyleBackColor = false;
+            this.btn_enemyemplacements.Paint += new System.Windows.Forms.PaintEventHandler(this.Button1_Paint);
+            this.btn_enemyemplacements.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Btn_enemyemplacements_MouseClick);
+            // 
+            // btn_RPGsquads
+            // 
+            this.btn_RPGsquads.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_RPGsquads.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_RPGsquads.Location = new System.Drawing.Point(44, 212);
+            this.btn_RPGsquads.Name = "btn_RPGsquads";
+            this.btn_RPGsquads.Size = new System.Drawing.Size(112, 93);
+            this.btn_RPGsquads.TabIndex = 7;
+            this.btn_RPGsquads.UseVisualStyleBackColor = false;
+            this.btn_RPGsquads.Paint += new System.Windows.Forms.PaintEventHandler(this.Btn_RPGsquads_Paint);
+            this.btn_RPGsquads.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Btn_RPGsquads_MouseClick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1466, 918);
+            this.Controls.Add(this.pnl_Obstacles);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.btn_close);
             this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.Click += new System.EventHandler(this.Form1_Click);
-            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.pnl_Obstacles.ResumeLayout(false);
+            this.pnl_Obstacles.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripProgressBar tlt_Fuel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnl_Obstacles;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_RPGsquads;
+        private System.Windows.Forms.Button btn_enemyemplacements;
     }
 }
 
--- military_simulations/Form1.cs
+++ military_simulations/Form1.cs
@@ -14,103 +14,227 @@ namespace military_simulations
 {
     public partial class Form1 : Form
     {
-        List<Rectangle> listOfObstacles = new List<Rectangle>();
-        
+        List<Rectangle> listOfEnemiesPlacements = new List<Rectangle>();
+        List<Rectangle> listOfRPGSquads = new List<Rectangle>();
+        List<Rectangle> listOfHomeBase = new List<Rectangle>();
+        List<Rectangle> listOfEnemyBase = new List<Rectangle>();
+
         private bool initialLoad = false;
+
+        //Home Base Points
+        Point hp1 = new Point(16, 14);
+        Point hp2 = new Point(16, 15);
+        Point hp3 = new Point(15, 14);
+        Point hp4 = new Point(15, 15);
+
+        //Enemy Base Points
+        Point ep1 = new Point(1, 1);
+        Point ep2 = new Point(1, 2);
+        Point ep3 = new Point(2, 1);
+        Point ep4 = new Point(2, 2);
 
         // Vairables
         int gridX;
         int gridY;
         int width = 50;
-        Rectangle[,] rectangles;
-        Color color;
+        int obstacleIndexX;
+        int obstacleIndexY;
+
+        // Variables for Selecting Obstacles
+        private bool enemyPlacementsClicked = false;
+        private bool rpgSquads = false;
+
+        Rectangle[,] rectanglesInitial;
+        Rectangle[,] rectanglesReDraw;      
+
+        private void PopulateHomeBase()
+        {
+            listOfHomeBase.Add(new Rectangle(hp1.X * width, hp1.Y * width, width, width));
+            listOfHomeBase.Add(new Rectangle(hp2.X * width, hp2.Y * width, width, width));
+            listOfHomeBase.Add(new Rectangle(hp3.X * width, hp3.Y * width, width, width));
+            listOfHomeBase.Add(new Rectangle(hp4.X * width, hp4.Y * width, width, width));
+        }
+
+        private void PopulateEnemyBase()
+        {
+            listOfEnemyBase.Add(new Rectangle(ep1.X * width, ep1.Y * width, width, width));
+            listOfEnemyBase.Add(new Rectangle(ep2.X * width, ep2.Y * width, width, width));
+            listOfEnemyBase.Add(new Rectangle(ep3.X * width, ep3.Y * width, width, width));
+            listOfEnemyBase.Add(new Rectangle(ep4.X * width, ep4.Y * width, width, width));
+        }
 
         public Form1()
         {
-            InitializeComponent();
-            color = Color.Black;
+            InitializeComponent();            
+            tlt_Fuel.Value = 100;
+            PopulateHomeBase();
+            PopulateEnemyBase();
         }
 
         private void Form1_Load(object sender, EventArgs e)
-        {
-            this.Paint += Form1_Paint;
-        }
-
-        private void Form1_Paint(object sender, PaintEventArgs e)
-        {
-            
-            
-        }
-
-        private void Form1_Click(object sender, EventArgs e)
-        {
-            
+        {            
         }
 
         private void Panel1_Paint(object sender, PaintEventArgs e)
         {
+            gridX = this.Width / width;
+            gridY = this.Height / width;
+            rectanglesInitial = new Rectangle[gridX, gridY];
+            // INITIAL DRAW
             if (initialLoad == false)
             {
                 initialLoad = true;
-                // Display Grid                
-                gridX = this.Width / width;
-                gridY = this.Height / width;
-                rectangles = new Rectangle[gridX, gridY];
+                // Display Grid                                                
                 for (int i = 0; i < gridX; i++)
                 {
                     for (int j = 0; j < gridY; j++)
                     {
                         Rectangle rec = new Rectangle(i * width, j * width, width, width);
-                        rectangles[i, j] = rec;
-                        e.Graphics.DrawRectangle(new Pen(color), rec);
-                        //Thread.Sleep(100);
+                        rectanglesInitial[i, j] = rec;
+                        e.Graphics.DrawRectangle(new Pen(Color.Black, 2), rec);                        
                     }
+                }                
+                foreach (var homeBase in listOfHomeBase)
+                {
+                    e.Graphics.DrawRectangle(new Pen(Color.LimeGreen, 3), homeBase);
                 }
-                Debug.WriteLine(rectangles.Length);
+                foreach (var enemyBase in listOfEnemyBase)
+                {
+                    e.Graphics.DrawRectangle(new Pen(Color.Red, 3), enemyBase);
+                }
+                Debug.WriteLine(rectanglesInitial.Length);
+                rectanglesReDraw = rectanglesInitial;
             }
+            // REDRAW
             else if (initialLoad == true)
             {
-                //Pen pen = new Pen(Color.Blue, 5);
-                //foreach (var i in listOfObstacles)
-                //{
-                //    e.Graphics.DrawRectangle(pen, i);
-                //}
-
-                // Display Grid                
-                gridX = this.Width / width;
-                gridY = this.Height / width;
-                rectangles = new Rectangle[gridX, gridY];
                 for (int i = 0; i < gridX; i++)
                 {
                     for (int j = 0; j < gridY; j++)
                     {
                         Rectangle rec = new Rectangle(i * width, j * width, width, width);
-                        rectangles[i, j] = rec;
-                        e.Graphics.DrawRectangle(new Pen(Color.Red), rec);
-                        //Thread.Sleep(100);
+                        rectanglesReDraw[i, j] = rec;
+                        e.Graphics.DrawRectangle(new Pen(Color.Black, 2), rec);
                     }
                 }
-                Debug.WriteLine(rectangles.Length);
+                foreach (var homeBase in listOfHomeBase)
+                {
+                    e.Graphics.DrawRectangle(new Pen(Color.LimeGreen, 3), homeBase);
+                }
+                foreach (var enemyBase in listOfEnemyBase)
+                {
+                    e.Graphics.DrawRectangle(new Pen(Color.Red, 3), enemyBase);
+                }
+                if (enemyPlacementsClicked == true)
+                {
+                    listOfEnemiesPlacements.Add(new Rectangle(obstacleIndexX * width, obstacleIndexY * width, width, width));
+                }
+                else if (rpgSquads == true)
+                {
+                    listOfRPGSquads.Add(new Rectangle(obstacleIndexX * width, obstacleIndexY * width, width, width));
+                }
+                
+                foreach (var enemies in listOfEnemiesPlacements)
+                {
+                    e.Graphics.DrawRectangle(new Pen(Color.LightSkyBlue, 3), enemies);
+                }
+                foreach (var rpg in listOfRPGSquads)
+                {
+                    e.Graphics.DrawRectangle(new Pen(Color.Orange, 3), rpg);
+                }
+                Debug.WriteLine(rectanglesReDraw.Length);
             }
         }
 
         private void Panel1_MouseClick(object sender, MouseEventArgs e)
         {
-            SetStyle(ControlStyles.ResizeRedraw, true);
-            for (int i = 0; i < gridX; i++)
+            if (enemyPlacementsClicked == true || rpgSquads == true)
             {
-                for (int j = 0; j < gridY; j++)
+                SetStyle(ControlStyles.ResizeRedraw, true);
+                for (int i = 0; i < gridX; i++)
                 {
-                    Point point = new Point(MousePosition.X, MousePosition.Y);
-                    if (rectangles[i, j].Contains(point))
+                    for (int j = 0; j < gridY; j++)
                     {
-                        MessageBox.Show(rectangles[i, j].X + "and" + rectangles[i, j].Y);
-                        rectangles[i, j] = new Rectangle(i * width, j * width, width, width);
-                        color = Color.Red;
+                        Point point = new Point(MousePosition.X, MousePosition.Y);
+                        if (rectanglesReDraw[i, j].Contains(point))
+                        {
+                            obstacleIndexX = i;
+                            obstacleIndexY = j;
+                        }
                     }
-                }
+                }                
+                panel1.Invalidate();
+                //MessageBox.Show(enemyPlacementsClicked + " " + rpgSquads);
             }
-            panel1.Invalidate();
-        }       
+            else if (enemyPlacementsClicked == false && rpgSquads == false)
+            {
+                MessageBox.Show("Please Select an Obstacle");
+            }            
+        }
+
+        private void Btn_close_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        #region BUTTON DRAW
+        private void Button1_Paint(object sender, PaintEventArgs e)
+        {
+            Point p = new Point(btn_enemyemplacements.Width / 2 - 25, btn_enemyemplacements.Height / 2 - 25);
+            Rectangle rec = new Rectangle(p.X, p.Y, 50, 50);
+            e.Graphics.DrawRectangle(new Pen(Color.LightSkyBlue, 2), rec);
+        }
+
+        private void Btn_RPGsquads_Paint(object sender, PaintEventArgs e)
+        {
+            Point p = new Point(btn_enemyemplacements.Width / 2 - 25, btn_enemyemplacements.Height / 2 - 25);
+            Rectangle rec = new Rectangle(p.X, p.Y, 50, 50);
+            e.Graphics.DrawRectangle(new Pen(Color.Orange, 2), rec);
+        }
+        #endregion
+
+        #region OBSTACLES CLICKED
+        private void Btn_enemyemplacements_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (enemyPlacementsClicked == false)
+            {
+                if (rpgSquads == true)
+                {
+                    btn_RPGsquads.BackColor = Color.White;
+                    btn_enemyemplacements.Focus();
+                    rpgSquads = false;
+                }
+                btn_enemyemplacements.BackColor = Color.Gray;                
+                enemyPlacementsClicked = true;                
+            }
+            else if (enemyPlacementsClicked == true)
+            {
+                enemyPlacementsClicked = false;
+                btn_enemyemplacements.BackColor = Color.White;
+                panel1.Focus();
+            }
+        }
+
+        private void Btn_RPGsquads_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (rpgSquads == false)
+            {
+                if (enemyPlacementsClicked == true)
+                {
+                    btn_enemyemplacements.BackColor = Color.White;
+                    btn_RPGsquads.Focus();
+                    enemyPlacementsClicked = false;
+                }
+                btn_RPGsquads.BackColor = Color.Gray;                
+                rpgSquads = true;
+            }
+            else if (rpgSquads == true)
+            {
+                rpgSquads = false;
+                btn_RPGsquads.BackColor = Color.White;
+                panel1.Focus();
+            }
+        }
+        #endregion
     }
 }
