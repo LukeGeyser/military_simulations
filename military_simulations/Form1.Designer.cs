62f8e9dc1261940c42677e9086bd1334920e9ae5
Luke Geyser <geyserlg@gmail.com>
2019-04-26 20:19:10 +0200

    Changes

--- military_simulations/Form1.Designer.cs
+++ military_simulations/Form1.Designer.cs
@@ -28,22 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(900, 851);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseClick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1422, 786);
+            this.ClientSize = new System.Drawing.Size(1466, 918);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Click += new System.EventHandler(this.Form1_Click);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- military_simulations/Form1.cs
+++ military_simulations/Form1.cs
@@ -2,31 +2,115 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace military_simulations
 {
     public partial class Form1 : Form
     {
+        List<Rectangle> listOfObstacles = new List<Rectangle>();
+        
+        private bool initialLoad = false;
+
+        // Vairables
+        int gridX;
+        int gridY;
+        int width = 50;
+        Rectangle[,] rectangles;
+        Color color;
+
         public Form1()
         {
             InitializeComponent();
+            color = Color.Black;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+            this.Paint += Form1_Paint;
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            Graphics gr = this.CreateGraphics();
-            Pen p = new Pen(Color.Green, 5);
-            gr.DrawRectangle(p, 50, 50, 200, 200);
+            
+            
         }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (initialLoad == false)
+            {
+                initialLoad = true;
+                // Display Grid                
+                gridX = this.Width / width;
+                gridY = this.Height / width;
+                rectangles = new Rectangle[gridX, gridY];
+                for (int i = 0; i < gridX; i++)
+                {
+                    for (int j = 0; j < gridY; j++)
+                    {
+                        Rectangle rec = new Rectangle(i * width, j * width, width, width);
+                        rectangles[i, j] = rec;
+                        e.Graphics.DrawRectangle(new Pen(color), rec);
+                        //Thread.Sleep(100);
+                    }
+                }
+                Debug.WriteLine(rectangles.Length);
+            }
+            else if (initialLoad == true)
+            {
+                //Pen pen = new Pen(Color.Blue, 5);
+                //foreach (var i in listOfObstacles)
+                //{
+                //    e.Graphics.DrawRectangle(pen, i);
+                //}
+
+                // Display Grid                
+                gridX = this.Width / width;
+                gridY = this.Height / width;
+                rectangles = new Rectangle[gridX, gridY];
+                for (int i = 0; i < gridX; i++)
+                {
+                    for (int j = 0; j < gridY; j++)
+                    {
+                        Rectangle rec = new Rectangle(i * width, j * width, width, width);
+                        rectangles[i, j] = rec;
+                        e.Graphics.DrawRectangle(new Pen(Color.Red), rec);
+                        //Thread.Sleep(100);
+                    }
+                }
+                Debug.WriteLine(rectangles.Length);
+            }
+        }
+
+        private void Panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            SetStyle(ControlStyles.ResizeRedraw, true);
+            for (int i = 0; i < gridX; i++)
+            {
+                for (int j = 0; j < gridY; j++)
+                {
+                    Point point = new Point(MousePosition.X, MousePosition.Y);
+                    if (rectangles[i, j].Contains(point))
+                    {
+                        MessageBox.Show(rectangles[i, j].X + "and" + rectangles[i, j].Y);
+                        rectangles[i, j] = new Rectangle(i * width, j * width, width, width);
+                        color = Color.Red;
+                    }
+                }
+            }
+            panel1.Invalidate();
+        }       
     }
 }
