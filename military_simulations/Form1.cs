8d1294f21e19efcce5335c50814f255e05aab4f2
Luke Geyser <geyserlg@gmail.com>
2019-04-26 12:12:57 +0200

    Added Obstacles

--- military_simulations/Form1.cs
+++ military_simulations/Form1.cs
@@ -16,5 +16,17 @@ namespace military_simulations
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics gr = this.CreateGraphics();
+            Pen p = new Pen(Color.Green, 5);
+            gr.DrawRectangle(p, 50, 50, 200, 200);
+        }
     }
 }
